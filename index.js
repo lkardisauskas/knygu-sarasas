b5be7a03d0fc4922eb66094ec65d288b8f559d42
lkardisauskas <lkardisauskas@gmail.com>
2023-04-18 19:01:50 +0300

    js +- done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.addEventListener("load", () => {
 btnAddBook.addEventListener("click", () => {
   const addBookInputs = document.getElementById("addBookInputs");
   addBookInputs.style.display =
-    addBookInputs.style.display === "none" ? "flex" : "none";
+    addBookInputs.style.display === "flex" ? "none" : "flex";
 });
 
 submitNewBook.addEventListener("click", (e) => {
@@ -44,35 +44,116 @@ submitNewBook.addEventListener("click", (e) => {
   displayBooks();
 });
 
-function displayBooks() {
+function displayBooks(searchTerm = "", authorFilter = null, sortType = null) {
   const bookshelf = document.getElementById("bookshelf");
-  books = JSON.parse(localStorage.getItem("books"));
-
+  const books = JSON.parse(localStorage.getItem("books"));
+  bookshelf.innerHTML = "";
   if (books) {
-    for (let i = 0; i < books.length; i++) {
-      let cardHTML = `    
-      <div class="card" style="width: 18rem">
-        <img
-          src="${books[i].url}"
-          class="card-img-top"
-          alt="..."
-        />
-        <div class="card-body">
-          <h5 class="card-title">${books[i].KnygosPavadinimas}</h5>
-          <p class="card-text">
-          ${books[i].KnygosAutorius}
-          </p>
+    let filteredBooks = books
+      .filter((book) =>
+        book.KnygosPavadinimas.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .filter((book) => {
+        if (authorFilter === null || authorFilter === "All books") {
+          return true;
+        }
+        // return book.KnygosAutorius === authorFilter;
+        return book.KnygosAutorius === authorFilter || (!authorFilter && book);
+      });
+
+    if (sortType === "lowToHigh") {
+      filteredBooks = filteredBooks.sort((a, b) => a.Kaina - b.Kaina);
+    } else if (sortType === "highToLow") {
+      filteredBooks = filteredBooks.sort((a, b) => b.Kaina - a.Kaina);
+    }
+
+    for (let i = 0; i < filteredBooks.length; i++) {
+      let cardHTML = `
+        <div class="card" id="card${i}" style="width: 18rem">
+          <img
+            src="${filteredBooks[i].url}"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">${filteredBooks[i].KnygosPavadinimas}</h5>
+            <p class="card-text">
+            ${filteredBooks[i].KnygosAutorius}
+            </p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Kategorija: <b> ${filteredBooks[i].Kategorija} </b></li>
+            <li class="list-group-item">Isleidimo metai: <b> ${filteredBooks[i].IsleidimoMetai} </b></li>
+          </ul>
+          <div class="card-body card-body-footer">
+            <h5>${filteredBooks[i].Kaina}$</h5>
+            <button class="btn btn-primary" data-book-index="${i}">Delete</button>
+          </div>
         </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Kategorija: <b> ${books[i].Kategorija} </b></li>
-          <li class="list-group-item">Isleidimo metai: <b> ${books[i].IsleidimoMetai} </b></li>
-        </ul>
-        <div class="card-body">
-          <h5>${books[i].Kaina}$</h5>
-        </div>
-      </div>
-      `;
+        `;
       bookshelf.innerHTML += cardHTML;
     }
   }
+
+  const deleteButtons = document.querySelectorAll(".card button");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const bookIndex = button.dataset.bookIndex;
+      books.splice(bookIndex, 1);
+      localStorage.setItem("books", JSON.stringify(books));
+      displayBooks(searchTerm, authorFilter);
+    });
+  });
 }
+
+function createFilterMenu() {
+  const books = JSON.parse(localStorage.getItem("books"));
+  const filterMenu = document.getElementById("filterMenu");
+
+  filterMenu.innerHTML = "";
+
+  const allBooksOption = document.createElement("li");
+  allBooksOption.innerHTML =
+    '<button class="dropdown-item" type="button">All books</button>';
+  filterMenu.appendChild(allBooksOption);
+
+  allBooksOption.addEventListener("click", () => {
+    displayBooks();
+  });
+
+  const knygosAutoriai = new Set();
+  for (let book of books) {
+    knygosAutoriai.add(book.KnygosAutorius);
+  }
+  for (let knygosAutorius of knygosAutoriai) {
+    const option = document.createElement("li");
+    option.innerHTML = `<button class="dropdown-item" type="button">${knygosAutorius}</button>`;
+    filterMenu.appendChild(option);
+
+    option.addEventListener("click", (e) => {
+      const authorFilter = e.target.textContent;
+      displayBooks("", authorFilter);
+    });
+  }
+}
+
+searchBar.addEventListener("input", () => {
+  const searchTerm = searchBar.value;
+  displayBooks(searchTerm);
+});
+
+sortLowHigh.addEventListener("click", (e) => {
+  displayBooks("", null, "lowToHigh");
+});
+
+sortHighLow.addEventListener("click", (e) => {
+  displayBooks("", null, "highToLow");
+});
+
+sortDefault.addEventListener("click", (e) => {
+  displayBooks("", null);
+});
+
+const authorFilter = "s";
+createFilterMenu();
+displayBooks("", authorFilter);
